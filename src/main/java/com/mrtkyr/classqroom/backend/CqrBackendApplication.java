f32836f6f4fd50a4ca0b6c936f04cf206bb8bd86
mertkiyar <mertkiyar69@gmail.com>
2026-01-27 19:12:22 +0300

    First commit

---
+++ src/main/java/com/mrtkyr/classqroom/backend/CqrBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mrtkyr.classqroom.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CqrBackendApplication.class, args);
+    }
+
+}
