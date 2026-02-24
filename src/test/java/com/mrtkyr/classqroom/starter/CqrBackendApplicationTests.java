fd6a54ea21f7dd995f4256316efe19962717fd2c
mertkiyar <mertkiyar69@gmail.com>
2026-02-24 15:58:22 +0300

    test: add unit tests for fetching `Student` and `Lecturer` by ID in `CqrBackendApplicationTests`

--- src/test/java/com/mrtkyr/classqroom/starter/CqrBackendApplicationTests.java
+++ src/test/java/com/mrtkyr/classqroom/starter/CqrBackendApplicationTests.java
@@ -1,13 +1,37 @@
 package com.mrtkyr.classqroom.starter;
 
+import com.mrtkyr.classqroom.dto.DtoLecturer;
+import com.mrtkyr.classqroom.dto.DtoStudent;
+import com.mrtkyr.classqroom.service.ILecturerService;
+import com.mrtkyr.classqroom.service.IStudentService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.UUID;
+
+@SpringBootTest(classes = {CqrBackendApplication.class})
 class CqrBackendApplicationTests {
 
+    @Autowired
+    private IStudentService studentService;
+
+    @Autowired
+    private ILecturerService lecturerService;
+
     @Test
-    void contextLoads() {
+    public void testGetStudentById() {
+        DtoStudent dtoStudent = studentService.getStudentById(UUID.fromString("1e4b8149-047d-40b6-a012-09b2bc049c9e"));
+        if (dtoStudent != null) {
+            System.out.println("Name : " + dtoStudent.getFirstName());
+        }
     }
 
+    @Test
+    public void testGetLecturerById() {
+        DtoLecturer dtoLecturer = lecturerService.getLecturerById(UUID.fromString("c1bec187-6119-441d-9a6f-ee368144ff05"));
+        if (dtoLecturer != null) {
+            System.out.println("Name : " + dtoLecturer.getFirstName());
+        }
+    }
 }
