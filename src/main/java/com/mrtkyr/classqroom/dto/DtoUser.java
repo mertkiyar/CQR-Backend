f288008d4f353609b4351bf06537bcaea893a44a
mertkiyar <mertkiyar69@gmail.com>
2026-06-16 23:19:03 +0300

    fix: add userId field for response quality and code performance

--- src/main/java/com/mrtkyr/classqroom/dto/DtoUser.java
+++ src/main/java/com/mrtkyr/classqroom/dto/DtoUser.java
@@ -5,12 +5,14 @@ import com.mrtkyr.classqroom.enums.UserType;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class DtoUser {
+    private UUID userId;
     private String firstName;
     private String lastName;
     private String email;
